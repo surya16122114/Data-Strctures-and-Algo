2c601bddfa84f67bda1492aabdad17a8ff395c49
Chinnasurya Prasad Vulavala <chinnasuryaprasad2001@gmail.com>
2025-12-30 23:20:11 -0500

    LIS

---
+++ LIS.java
@@ -0,0 +1,57 @@
+/*
+300. Longest Increasing Subsequence
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+
+
+Leetcode problem link: https://leetcode.com/problems/longest-increasing-subsequence/
+*/
+
+package DataStructuresAndAlgo;
+
+public class LIS {
+    class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        int[] dp =new int[n];
+      for(int i=0; i<n; i++) dp[i]=1;
+        for(int i=n-2; i>=0; i--){
+            for(int j=i+1; j<n; j++){
+                if(nums[i]<nums[j])
+                dp[i]=Math.max(dp[i], 1+dp[j]);
+            }
+        }
+int res=0;
+        for(int i=0; i<n; i++)
+        res=Math.max(res, dp[i]);
+
+        return res;
+    }
+}
+}

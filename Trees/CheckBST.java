7b1f55bb8daddbed9ab9c82d0d9f16edf6101dc6
Chinnasurya Prasad Vulavala <chinnasuryaprasad2001@gmail.com>
2026-02-01 17:53:22 -0500

    check bst

---
+++ Trees/CheckBST.java
@@ -0,0 +1,32 @@
+package DataStructuresAndAlgo.Trees;
+
+public class CheckBST {
+    class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return checkBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        
+    }
+
+    public boolean checkBST(TreeNode root, long mini, long maxi){
+        if(root==null)
+        return true;
+
+        if(root.val<=mini || root.val>=maxi )
+        return false;
+    return  checkBST(root.left, mini, root.val) && checkBST(root.right, root.val, maxi);
+
+    }
+}
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+}

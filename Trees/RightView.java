b4491ff61ecbdca497d223db3a17c2c7b273f993
Chinnasurya Prasad Vulavala <chinnasuryaprasad2001@gmail.com>
2026-02-01 18:10:40 -0500

    Right view of binary tree

---
+++ Trees/RightView.java
@@ -0,0 +1,45 @@
+package DataStructuresAndAlgo.Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class RightView {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> ans=new ArrayList<>();
+        if(root==null)
+        return ans;
+        Queue<TreeNode> q=new LinkedList<>();
+    
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0; i<size; i++){
+                TreeNode curr=q.poll();
+                if(i==0)
+                ans.add(curr.val);
+                if(curr.right!=null)
+                q.offer(curr.right);
+                if(curr.left!=null)
+                q.offer(curr.left);
+            }
+            
+        }
+        return ans;
+    }
+}
+}

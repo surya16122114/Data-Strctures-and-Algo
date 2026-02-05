1426da2d81a784b03cab1752751f5e7292ab01b0
Chinnasurya Prasad Vulavala <chinnasuryaprasad2001@gmail.com>
2026-02-04 19:31:37 -0500

    Container with most rain water

---
+++ TwoPointers/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+public class ContainerWithMostWater {
+    class Solution {
+        public int maxArea(int[] height) {
+    
+            int left=0;
+            int right=height.length-1;
+    
+            int area=0;
+    
+            while(left<right){
+                area=Math.max(area, Math.min(height[left], height[right])*(right-left));
+    
+                if(height[left]<height[right])
+                left++;
+                else
+                right--;
+            }
+    
+            return area;
+            
+        }
+    }
+}

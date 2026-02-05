eb4fe9693787cd7ec7ce8a0e51a4572dd8291de9
Chinnasurya Prasad Vulavala <chinnasuryaprasad2001@gmail.com>
2026-02-04 21:30:06 -0500

    Trap Rain Water

---
+++ TwoPointers/TrapRainWater.java
@@ -0,0 +1,35 @@
+public class TrapRainWater {
+    class Solution {
+        public int trap(int[] height) {
+            int res=0;
+            int low=0;
+            int high=height.length-1;
+    
+            int left=height[0];
+            int right=height[high];
+    
+    
+            while(low<high){
+    
+                if(left<right){
+                    low++;
+                    left=Math.max(left, height[low]);
+                    res=res+left-height[low];
+                }
+    
+                else{
+                    high--;
+                    right=Math.max(right, height[high]);
+                    res=res+right-height[high];
+                }
+            }
+    
+    
+            return res;
+    
+    
+    
+    
+        }
+    }
+}

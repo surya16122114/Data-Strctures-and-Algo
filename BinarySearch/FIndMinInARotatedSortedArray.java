ee2f32b34d04f039f2f5a14b9083495882e36491
Chinnasurya Prasad Vulavala <chinnasuryaprasad2001@gmail.com>
2026-02-07 17:14:44 -0500

    Min In a Rotated Sorted Array

---
+++ BinarySearch/FIndMinInARotatedSortedArray.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class FIndMinInARotatedSortedArray {
+    class Solution {
+        public int findMin(int[] nums) {
+            int ans=Integer.MAX_VALUE;
+            
+            int left=0;
+            int right=nums.length-1;
+            while(left<=right){
+                int mid=left+(right-left)/2;
+    
+                if(nums[left]<=nums[mid]){
+                    ans=Math.min(ans, nums[left]);
+                    left=mid+1;
+                }
+                else
+                {
+                    ans=Math.min(ans, nums[mid]);
+                    right=mid-1;
+                }
+            }
+    
+    
+            return ans;
+        }
+    }
+}

af77383caf6e4459718a30abc7ca2d58618d3a00
surya16122114 <chinnasuryaprasad2001@gmail.com>
2026-02-14 14:38:02 -0500

    Valid Paranthesis

---
+++ Stacks/ValidParanthesis.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class ValidParanthesis {
+    class Solution {
+    public boolean isValid(String s) {
+        
+
+    Stack<Character> stack=new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
+                stack.push(ch);
+            }
+
+            else{
+                if(stack.isEmpty())
+                return false;
+
+
+                char temp=stack.pop();
+
+                if(ch==')' && temp=='(' || ch=='}' && temp=='{' || ch==']' && temp=='[')
+                continue;
+
+                else
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+
+}

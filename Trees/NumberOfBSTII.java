1f4462c418ab35c1e8464a3ebe8a44e903ba1194
surya16122114 <chinnasuryaprasad2001@gmail.com>
2026-02-20 16:37:50 -0500

    Unique Binary Search Trees

---
+++ Trees/NumberOfBSTII.java
@@ -0,0 +1,57 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberOfBSTII {
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+
+        return generate(1,n);
+        
+    }
+
+    public List<TreeNode> generate(int start, int end) {
+        List<TreeNode> result=new ArrayList<>();
+
+        if(start>end){
+            result.add(null);
+            return result;
+        }
+
+
+
+        for(int i=start; i<=end; i++){
+            List<TreeNode> leftTrees=generate(start,i-1);
+            List<TreeNode> rightTrees=generate(i+1, end);
+
+
+            for(TreeNode left: leftTrees){
+                for(TreeNode right: rightTrees){
+                    TreeNode root=new TreeNode(i);
+                    root.left=left;
+                    root.right=right;
+                    result.add(root);
+                }
+            }
+        }
+
+
+
+        return result;
+    }
+} 
+}

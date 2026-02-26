3797fcadce70ca20ee030a8be1d320e5b593dfb0
surya16122114 <chinnasuryaprasad2001@gmail.com>
2026-02-26 17:10:50 -0500

    Wild Card Matching

---
+++ DP/WildCardMatching.java
@@ -0,0 +1,39 @@
+package DP;
+
+public class WildCardMatching {
+    class Solution {
+        public boolean isMatch(String s, String p) {
+            int m=s.length();
+            int n=p.length();
+            boolean[][] dp=new boolean[m+1][n+1];
+    
+            dp[0][0]=true;
+    //empty matching with *
+            for(int j=1; j<=n ;j++){
+                if(p.charAt(j-1)=='*')
+                dp[0][j]=dp[0][j-1];
+    
+            }
+            for(int i=1; i<=m; i++){
+                for(int j=1; j<=n; j++){
+    
+                    //case-1: if they are equal
+                    if(s.charAt(i-1)==p.charAt(j-1)  || p.charAt(j-1)=='?')
+                    {
+                        dp[i][j]=dp[i-1][j-1];
+                    }
+    
+                    else if( p.charAt(j-1)=='*'){
+                        dp[i][j]=dp[i-1][j]|| dp[i][j-1];
+                    }
+                   
+                
+                }
+            }
+    
+    
+            return dp[m][n];
+            
+        }
+    }
+}
